21104a066ef18854d838050db68502a2d441a0f5
Aaron Philip <aaronphilip2003@gmail.com>
2022-07-03 12:34:38 +0530

    Completed Simple Storage implemented using brownie

---
+++ scripts/read_value.py
@@ -0,0 +1,12 @@
+from asyncore import read
+from brownie import SimpleStorage,accounts,config
+
+def read_contract():
+    simple_storage=SimpleStorage[-1]
+    # ABI
+    # address
+    print(simple_storage.retrieve())
+
+
+def main():
+    read_contract()
---
+++ tests/test_simple_storage.py
@@ -0,0 +1,22 @@
+from unittest import expectedFailure
+from brownie import SimpleStorage,accounts
+
+def test_deploy():
+    # Arrange
+    account=accounts[0]
+    # Act
+    simple_storage=SimpleStorage.deploy({"from":account})
+    starting_val=simple_storage.retrieve()
+    expected_value=0
+    # Assert
+    assert starting_val==expected_value
+
+def test_updated_storage():
+    # Arrange
+    account=accounts[0]
+    simple_storage=SimpleStorage.deploy({"from":account})
+    # Act
+    expected=15
+    simple_storage.store(15,{"from":account})
+    # Assert
+    assert expected==simple_storage.retrieve()
